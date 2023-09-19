fb877b2ee104cd3d1d168eacfc79366d0dc88791
kpollackhinds <kahlil.pollack-hinds@stonybrook.edu>
2023-09-18 23:59:56 -0400

    got camera feed in opencv

--- object_detection/inference.py
+++ object_detection/inference.py
@@ -1,5 +1,24 @@
 import cv2
 
 url = 'http://192.168.1.164/stream'
-cv2.namedWindow("ESP32 Wrover Cam Stream", cv2.WINDOW_AUTOSIZE)
 
+cap = cv2.VideoCapture(url)
+
+#check if camera open successfully
+if (cap.isOpened() == False):
+    print("Error opening video on stream")
+    exit()
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+
+        cv2.imshow('ESP32 Wrover Cam Stream', cv2.flip(frame, 0))
+        
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
